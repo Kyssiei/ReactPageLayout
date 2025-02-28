bf7cb62524186c1f8880ed091bbf1f7d30616a39
kyssiei <kyssiei.kay@gmail.com>
2025-02-28 00:18:07 -0500

    fixed employeeList foundation

--- src/components/EmployeeList.jsx
+++ src/components/EmployeeList.jsx
@@ -3,9 +3,19 @@ import EmployeeListItem from "./EmployeeListItem.jsx"
 function EmployeeList() {
     return(
         <div style={{border: "2px solid red"}}>
-        <EmployeeListItem name="James King" />
-        <EmployeeListItem name="Julie Taylor"/>
-        <EmployeeListItem name="Eugene Lee"/>
+            <EmployeeListItem name="James King" title="President and CEO"></EmployeeListItem>
+            <hr />
+            <EmployeeListItem name="Julie Taylor" title="VP of Marketing"></EmployeeListItem>
+            <hr />
+            <EmployeeListItem name="Eugene Lee" title="CFO"/>
+            <hr />
+            <EmployeeListItem name="John Wiiliams" title="VP of Engineering"></EmployeeListItem>
+            <hr />
+            <EmployeeListItem name="Ray Moore" title="VP of Sales"></EmployeeListItem>
+            <hr />
+            <EmployeeListItem name="Paul Jones" title="QA Manager"></EmployeeListItem>
+            <hr />
+        
         </div>
     )
 }
--- src/components/EmployeeListItem.jsx
+++ src/components/EmployeeListItem.jsx
@@ -1,16 +1,19 @@
 // img + name + job title 
 
+
+
 function EmployeeListItem(props){
 return(
-    <>
-        {/* <img src="https://via.placeholder.com/150" alt="profile picture"></img> */}
-        <img src="https://via.placeholder.com/150" alt="profile picture">{props.img}</img>
-        <h3 style={{fontFamily: "Arial, sans-serif", fontSize: "10px", fontWeight: "4"}}>{props.name}</h3>
-        <p>{props.title}</p>
-        
-    </>
+    <div style={{display: "flex", flexDirection: "row", gap: "0px"}}>
+        <img src={props.img || "https://via.placeholder.com/150"} alt="profile picture" style={{width:"50px", height: "50px", borderRadius: "50%"}}></img>
+        <div style={{display: "flex", flexDirection: "column"}}>
+            <p style={{fontFamily: "Arial, sans-serif", fontSize: "18px", fontWeight: "100", margin: "0px"}}>{props.name}</p>
+            <p style={{fontFamily: "arial, sans-serif", color: "grey", fontSize: "14px", margin: "0px"}}>{props.title}</p>
+        </div>
+    </div>
 )
 }
+
 
 
 export default EmployeeListItem
